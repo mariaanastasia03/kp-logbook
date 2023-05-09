0f5bb82720a94baf44105f6d27efb0c6653f6367
mariaanastasia03 <mariatambunan03@gmail.com>
2023-05-09 22:43:08 +0700

    complete frontend

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,35 @@
+import React from 'react';
+import './styles.css';
+
 export default function Index() {
   return (
-    <div className=" overflow-hidden">
-      <>This is home</>
+    <div className="center">
+      <div className="container">
+        <div className="box total-device">
+          Total Device <br />
+          1000
+        </div>
+        <div className="box total-assets">
+          Total Assets <br />
+          1000
+        </div>
+        <div className="box available">
+          In Stock <br />
+          1000
+        </div>
+        <div className="box used">
+          In Use <br />
+          1000
+        </div>
+        <div className="box repairing">
+          Missing <br />
+          1000
+        </div>
+        <div className="box broken">
+          Retired (or consumed) <br />
+          1000
+        </div>
+      </div>
     </div>
   );
 }
